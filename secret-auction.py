fe4741cc7ad4f397f19043f151aed4f48e6baacf
Arkadiusz Galas <arkadiuszgalas@macbook-air-arkadiusz-1.home>
2025-10-24 21:49:59 +0200

    Initial commit starting screen

---
+++ secret-auction.py
@@ -0,0 +1,6 @@
+print("Welcome to the secret auction program.")
+name_player = input("What is your name ?: ")
+bid = input("What's your bid ?: $")
+
+bids_table = dict()
+bids_table[name_player] = bid
