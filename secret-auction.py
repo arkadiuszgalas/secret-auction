f4769afb2028d9fcf19099d352348dcc19d99e12
Arkadiusz Galas <arkadiusz.galas@gmail.com>
2025-10-27 21:29:28 +0100

    Adding while loop allowing processing other bidders

--- secret-auction.py
+++ secret-auction.py
@@ -1,6 +1,14 @@
-print("Welcome to the secret auction program.")
-name_player = input("What is your name ?: ")
-bid = input("What's your bid ?: $")
+continue_bids = True
 
 bids_table = dict()
-bids_table[name_player] = bid
+while continue_bids:
+   print("Welcome to the secret auction program.")
+   name_player = input("What is your name ?: ")
+   bid = input("What's your bid ?: $")
+   bids_table[name_player] = bid
+   other = input("Are there any other bidders ? Type 'yes' or 'no'.")
+
+   if other == 'yes':
+      print("\n" * 5)
+   else:
+      continue_bids = False
