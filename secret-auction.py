e34e8f8e9c8ccd151737688f3c1b9163086800d0
Arkadiusz Galas <arkadiusz.galas@gmail.com>
2025-10-28 23:56:58 +0100

    Selecting the highest bid from the dictionary

--- secret-auction.py
+++ secret-auction.py
@@ -1,14 +1,24 @@
 continue_bids = True
 
 bids_table = dict()
+print("Welcome to the secret auction program.")
 while continue_bids:
-   print("Welcome to the secret auction program.")
    name_player = input("What is your name ?: ")
    bid = input("What's your bid ?: $")
-   bids_table[name_player] = bid
-   other = input("Are there any other bidders ? Type 'yes' or 'no'.")
+   bids_table[name_player] = int(bid)
+   other = input("Are there any other bidders ? Type 'yes' or 'no' \n")
 
    if other == 'yes':
-      print("\n" * 5)
+      print("\n" * 30)
    else:
       continue_bids = False
+
+max_bid = 0
+winner_name = ''
+for key in bids_table:
+   if bids_table[key] > max_bid:
+      max_bid = bids_table[key]
+      winner_name = key
+
+print("\n" * 30)
+print(f"The winner is {winner_name} with a bid of ${max_bid}.")
